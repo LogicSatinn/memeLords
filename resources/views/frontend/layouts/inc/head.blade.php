730e36a2dff4b4571acf2789231ac2dc1c54765a
Calvin Jackson <amigohmccclurkin98@gmail.com>
2022-05-15 23:30:41 +0300

    #6 the commit with the layout

---
+++ resources/views/frontend/layouts/inc/head.blade.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Favicon -->
+    <link href="{{ asset('assets/images/favicon.png')}}" rel="icon" type="image/png">
+
+    <!-- Basic Page Needs
+        ================================================== -->
+    <title>Socialite Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Socialite is - Professional A unique and beautiful collection of UI elements">
+
+    <!-- icons
+    ================================================== -->
+    <link rel="stylesheet" href="{{ asset('assets/css/icons.css')}}">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="{{ asset('assets/css/uikit.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+{{--    <link href="../../unpkg.com/tailwindcss%402.2.19/dist/tailwind.min.css" rel="stylesheet">--}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+</head>
---
+++ resources/views/frontend/layouts/inc/sidechat.blade.php
@@ -0,0 +1,165 @@
+<div class="sidechat">
+    <div class="sidechat_inner" data-simplebar>
+
+        <div class="absolute bg-white z-10 w-full -mt-5 lg:-mt-2 transform translate-y-1.5 py-2 border-b items-center flex"
+             id="search" hidden>
+            <input type="text" placeholder="Search.." class="flex-1">
+            <ion-icon name="close-outline" class="text-2xl hover:bg-gray-100 p-1 rounded-full mr-4 cursor-pointer"
+                      uk-toggle="target: #search;animation: uk-animation-slide-top-small"></ion-icon>
+        </div>
+
+        <nav class="responsive-nav border-b extanded mb-2 -mt-2 sidechat_tabs">
+            <ul uk-switcher="connect: #chats-tab; animation: uk-animation-fade">
+                <li class="uk-active"><a class="active" href="#0"> Friends </a></li>
+                <li><a href="#0">Groups <span> 10 </span> </a></li>
+            </ul>
+        </nav>
+
+        <div class="contact-list px-2 uk-switcher" id="chats-tab">
+
+            <div class="p-1">
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-7.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username"> Alex Dolgove</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-8.jpg')}}" alt="">
+                        <span class="user_status status_online"></span>
+                    </div>
+                    <div class="contact-username"> Dennis Han</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-2.jpg')}}" alt="">
+                        <span class="user_status"></span>
+                    </div>
+                    <div class="contact-username"> Erica Jones</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-3.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username">Stella Johnson</div>
+                </a>
+
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-5.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username">Adrian Mohani </div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-6.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username"> Jonathan Madano</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-2.jpg')}}" alt="">
+                        <span class="user_status"></span>
+                    </div>
+                    <div class="contact-username"> Erica Jones</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-1.jpg')}}" alt="">
+                        <span class="user_status status_online"></span>
+                    </div>
+                    <div class="contact-username"> Dennis Han</div>
+                </a>
+
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-5.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username">Adrian Mohani </div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-6.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username"> Jonathan Madano</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-2.jpg')}}" alt="">
+                        <span class="user_status"></span>
+                    </div>
+                    <div class="contact-username"> Erica Jones</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-1.jpg')}}" alt="">
+                        <span class="user_status status_online"></span>
+                    </div>
+                    <div class="contact-username"> Dennis Han</div>
+                </a>
+
+            </div>
+            <div class="p-1">
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-7.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username"> Alex Dolgove</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-8.jpg')}}" alt="">
+                        <span class="user_status status_online"></span>
+                    </div>
+                    <div class="contact-username"> Dennis Han</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-2.jpg')}}" alt="">
+                        <span class="user_status"></span>
+                    </div>
+                    <div class="contact-username"> Erica Jones</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-3.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username">Stella Johnson</div>
+                </a>
+
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-5.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username">Adrian Mohani </div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-6.jpg')}}" alt="">
+                    </div>
+                    <div class="contact-username"> Jonathan Madano</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-2.jpg')}}" alt="">
+                        <span class="user_status"></span>
+                    </div>
+                    <div class="contact-username"> Erica Jones</div>
+                </a>
+                <a href="chats-friend.html">
+                    <div class="contact-avatar">
+                        <img src="{{ asset('assets/images/avatars/avatar-1.jpg')}}" alt="">
+                        <span class="user_status status_online"></span>
+                    </div>
+                    <div class="contact-username"> Dennis Han</div>
+                </a>
+
+
+            </div>
+
+        </div>
+
+    </div>
+    <div class="sidechat_overly" uk-toggle="target: .sidechat; cls:active"></div>
+</div>
