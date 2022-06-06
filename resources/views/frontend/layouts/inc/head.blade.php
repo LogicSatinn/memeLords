939648e6b624f04325e77f91622f9bc72e511386
Calvin Jackson <69092766+LogicSatinn@users.noreply.github.com>
2022-06-07 01:12:54 +0300

    Update head.blade.php

--- resources/views/frontend/layouts/inc/head.blade.php
+++ resources/views/frontend/layouts/inc/head.blade.php
@@ -2,9 +2,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Favicon -->
-    <link href="{{ asset('favicon_package/favicon.ico')}}" rel="icon" >
-
     <!-- Basic Page Needs
         ================================================== -->
     <title>{{ config('app.name') }}</title>
