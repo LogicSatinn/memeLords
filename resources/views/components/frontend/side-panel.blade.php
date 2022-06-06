18c4d07476ba6ff2b5c48155ca3861080ac0505f
Calvin Jackson <69092766+LogicSatinn@users.noreply.github.com>
2022-06-06 22:03:14 +0300

    this commit contains heavy regrets on having to drop the socialite authentication feature (#24)

--- resources/views/components/frontend/side-panel.blade.php
+++ resources/views/components/frontend/side-panel.blade.php
@@ -1,28 +1,21 @@
 <div class="lg:w-72 w-full">
 
-    <a href="#birthdays" uk-toggle>
-        <div class="bg-white mb-5 px-4 py-3 rounded-md shadow">
-            <h3 class="text-line-through font-semibold mb-1"> Birthdays </h3>
-            <div class="-mx-2 duration-300 flex hover:bg-gray-50 px-2 py-2 rounded-md">
-                <img src="{{ asset('assets/images/icons/gift-icon.png')}}" class="w-9 h-9 mr-3" alt="">
-                <p class="line-clamp-2 leading-6"><strong> Jessica Erica </strong> and <strong> two
-                        others </strong>
-                    have a birthdays to day .
-                </p>
-            </div>
-        </div>
-    </a>
+{{--    <a href="#birthdays" uk-toggle>--}}
+{{--        <div class="bg-white mb-5 px-4 py-3 rounded-md shadow">--}}
+{{--            <h3 class="text-line-through font-semibold mb-1"> Birthdays </h3>--}}
+{{--            <div class="-mx-2 duration-300 flex hover:bg-gray-50 px-2 py-2 rounded-md">--}}
+{{--                <img src="{{ asset('assets/images/icons/gift-icon.png')}}" class="w-9 h-9 mr-3" alt="">--}}
+{{--                <p class="line-clamp-2 leading-6"><strong> Jessica Erica </strong> and <strong> two--}}
+{{--                        others </strong>--}}
+{{--                    have a birthdays to day .--}}
+{{--                </p>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </a>--}}
 
-    <h3 class="text-xl font-semibold"> Contacts </h3>
+    <h3 class="text-xl font-semibold"> Topics </h3>
 
-    <div class="" uk-sticky="offset:80">
-
-        <nav class="responsive-nav border-b extanded mb-2 -mt-2">
-            <ul uk-switcher="connect: #group-details; animation: uk-animation-fade">
-                <li class="uk-active"><a class="active" href="#0"> Friends <span> 310 </span> </a></li>
-                <li><a href="#0">Groups</a></li>
-            </ul>
-        </nav>
+    <div class="p-1" uk-sticky="offset:80">
 
         <div class="contact-list">
 
