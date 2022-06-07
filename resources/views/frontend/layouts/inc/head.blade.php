efd75a4d6a6555398cbbd737a957c2706ebe94c7
Calvin Jackson <amigohmccclurkin98@gmail.com>
2022-06-07 22:23:37 +0300

    getting ready for production

--- resources/views/frontend/layouts/inc/head.blade.php
+++ resources/views/frontend/layouts/inc/head.blade.php
@@ -7,14 +7,14 @@
     <title>{{ config('app.name') }}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-{{--    <meta name="description" content="Socialite is - Professional A unique and beautiful collection of UI elements">--}}
+    <meta name="description" content="What's cracking you up? Show us!!">
 
 {{--    Favicon Files--}}
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon_package/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon_package/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon_package/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{ asset('favicon_package/site.webmanifest')}}">
-    <link rel="mask-icon" href="{{ asset('favicon_package/safari-pinned-tab.svg')}}" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{ asset('/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{ asset('/safari-pinned-tab.svg')}}" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
