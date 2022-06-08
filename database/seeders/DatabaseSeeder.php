220ee37b58fcb02ff84083d7e8fa3b6b519b213f
Calvin Jackson <amigohmccclurkin98@gmail.com>
2022-06-08 11:38:57 +0300

    update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
@@ -19,7 +21,10 @@ class DatabaseSeeder extends Seeder
          $user = User::create([
              'name' => 'Super Admin',
              'email' => 'admin@memelords.app',
-             'username' => 'admin'
+             'username' => 'admin',
+             'email_verified_at' => now(),
+             'password' => Hash::make('maliksonneli'),
+             'remember_token' => Str::random(10),
          ]);
 
         Permission::create(['name' => 'access dashboard']);
